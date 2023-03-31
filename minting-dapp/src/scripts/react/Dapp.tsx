a88dadf9ad3ddfa7e128bd1019a00a174e3b40c9
PBNC23 <pitbullnftclub@gmail.com>
2023-03-31 23:43:40 +0400

    old88

--- minting-dapp/src/scripts/react/Dapp.tsx
+++ minting-dapp/src/scripts/react/Dapp.tsx
@@ -242,9 +242,10 @@ export default class Dapp extends React.Component<Props, State> {
             {!this.isWalletConnected() ? <button className="primary" disabled={this.provider === undefined} onClick={() => this.connectWallet()}>Connect Wallet</button> : null}
 
             <div className="use-block-explorer">
-              <h1> Welcome to Official Pitbull Nft Club Dapp</h1>
-              
-               
+              Hey, looking for a <strong>super-safe experience</strong>? <span className="emoji">😃</span><br />
+              You can interact with the smart-contract <strong>directly</strong> through <a href={this.generateContractUrl()} target="_blank">{this.state.networkConfig.blockExplorer.name}</a>, without even connecting your wallet to this DAPP! <span className="emoji">🚀</span><br />
+              <br />
+              Keep safe! <span className="emoji">❤️</span>
             </div>
 
             {!this.isWalletConnected() || this.state.isWhitelistMintEnabled ?
